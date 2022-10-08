aa56bbc69a8681f90c59c99c369b9351afeea035
Sébastien Bertrand <sebastien.bertrand@outlook.com>
2022-10-08 16:29:52 +0200

    AsyncRepository: Fix collection name generation

--- src/Infrastructure/GoalLessMeat.Persistence/Repositories/AsyncRepository.cs
+++ src/Infrastructure/GoalLessMeat.Persistence/Repositories/AsyncRepository.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using GoalLessMeat.AppLogic.Contracts.Persistence;
 using GoalLessMeat.Domain.Common;
+using GoalLessMeat.Persistence;
 using LiteDB;
 
 namespace GoalLessMeat.Persistence.Repositories
@@ -12,11 +13,11 @@ namespace GoalLessMeat.Persistence.Repositories
     public class AsyncRepository<T> : IAsyncRepository<T> where T : IAggregateRoot
     {
         private string DB_PATH = Path.Combine(FileSystem.AppDataDirectory, "GoalLessMeat.db");
+        private readonly string _collectionName = typeof(T).Name + "s";
         public async Task<T> GetByIdAsync(Guid id)
         {
             using var db = new LiteDatabase(DB_PATH);
-            var collectionName = nameof(T) + "s";
-            var collection = db.GetCollection<T>(collectionName);
+            var collection = db.GetCollection<T>(_collectionName);
             var result = collection.Query().Where(entity => entity.Id == id).SingleOrDefault();
             return result;
         }
@@ -24,8 +25,7 @@ namespace GoalLessMeat.Persistence.Repositories
         public async Task<IReadOnlyList<T>> ListAllAsync()
         {
             using var db = new LiteDatabase(DB_PATH);
-            var collectionName = nameof(T) + "s";
-            var collection = db.GetCollection<T>(collectionName);
+            var collection = db.GetCollection<T>(_collectionName);
             var results = collection.Query().ToList();
             return results;
         }
@@ -33,8 +33,7 @@ namespace GoalLessMeat.Persistence.Repositories
         public async Task<T> AddAsync(T entity)
         {
             using var db = new LiteDatabase(DB_PATH);
-            var collectionName = nameof(T) + "s";
-            var collection = db.GetCollection<T>(collectionName);
+            var collection = db.GetCollection<T>(_collectionName);
             collection.Insert(entity);
             return entity;
         }
@@ -42,16 +41,14 @@ namespace GoalLessMeat.Persistence.Repositories
         public async Task UpdateAsync(T entity)
         {
             using var db = new LiteDatabase(DB_PATH);
-            var collectionName = nameof(T) + "s";
-            var collection = db.GetCollection<T>(collectionName);
+            var collection = db.GetCollection<T>(_collectionName);
             collection.Update(entity);
         }
 
         public async Task DeleteAsync(T entity)
         {
             using var db = new LiteDatabase(DB_PATH);
-            var collectionName = nameof(T) + "s";
-            var collection = db.GetCollection<T>(collectionName);
+            var collection = db.GetCollection<T>(_collectionName);
             collection.Delete(entity.Id);
         }
     }
